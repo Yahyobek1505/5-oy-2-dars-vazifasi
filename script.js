34d59a450dc9045347fcf42dad0a052cafb1a58c
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-01-13 22:51:47 +0500

    Homework is started!

---
+++ script.js
@@ -0,0 +1,29 @@
+// import {kop as jami}   from "./function.js";
+
+// console.log(jami(10, 5));
+
+
+// let arr = [ "Odil", "Hamid", "Muslim", "Jamshid", "Asror"]
+
+// let car = {
+//   id: 2,
+//   name: "Damas",
+//   year: 2023,
+//   color: "oq", 
+//   type: "chevro",
+//   weight: 2000
+// };
+
+// let {type, color, id: num = 40, year: yosh = 2030, ...res} = car
+
+// console.log(num);
+
+
+// let salom = document.getElementById ("h");
+
+// salom.innerHTML = `Bye`;
+
+const text = document.getElementsByTagName ('p');
+const text2 = document.querySelectorAll ('p');
+
+console.log(text2);
